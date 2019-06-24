db61deda07e164acc39a3ec9eba2bb4691516f47
AndyEngland521 <andrew.england@sparkfun.com>
2019-06-24 13:53:41 -0600

    adding in RV3028

--- SparkFun_Qwiic_OpenLog_Arduino_Library.h
+++ SparkFun_Qwiic_OpenLog_Arduino_Library.h
@@ -31,7 +31,7 @@
 #define STATUS_LAST_COMMAND_KNOWN 2
 #define STATUS_FILE_OPEN 3
 #define STATUS_IN_ROOT_DIRECTORY 4
-
+  
 //Platform specific configurations
 
 //Define the size of the I2C buffer based on the platform the user has
@@ -40,6 +40,19 @@
 //The catch-all default is 32
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+#define TIME_ARRAY_LENGTH 7 // Total number of writable values in device
+
+enum time_order {
+	TIME_SECONDS,    // 0
+	TIME_MINUTES,    // 1
+	TIME_HOURS,      // 2
+	TIME_DAY,
+	TIME_DATE,       // 3
+	TIME_MONTH,      // 4
+	TIME_YEAR,       // 5
+};
 
 class OpenLog{
 
@@ -72,12 +85,68 @@ class OpenLog{
     uint32_t removeDirectory(char *thingToDelete); //Remove a directory including the contents of the directory
     uint32_t remove(char *thingToDelete, bool removeEverthing); //Remove file or directory including the contents of the directory
 
+	//RV3028 Functions
+	void setTime(uint8_t sec, uint8_t min, uint8_t hour, uint8_t date, uint8_t month, uint8_t year, uint8_t day);
+	void setTime(uint8_t * time, uint8_t len);
+	void setSeconds(uint8_t value);
+	void setMinutes(uint8_t value);
+	void setHours(uint8_t value);
+	void setDate(uint8_t value);
+	void setWeekday(uint8_t value);
+	void setMonth(uint8_t value);
+	void setYear(uint8_t value);
+	
+	void updateTime(); //Update the local array with the RTC registers
+	
+	char* stringDateUSA(); //Return date in mm-dd-yyyy
+	char* stringDate(); //Return date in dd-mm-yyyy
+	char* stringTime(); //Return time hh:mm:ss with AM/PM if in 12 hour mode
+	char* stringTimeStamp(); //Return timeStamp in ISO 8601 format yyyy-mm-ddThh:mm:ss
+	
+	uint8_t getSeconds();
+	uint8_t getMinutes();
+	uint8_t getHours();
+	uint8_t getDate();
+	uint8_t getWeekday();
+	uint8_t getMonth();
+	uint8_t getYear();	
+	
+	void setToCompilerTime(); //Uses the hours, mins, etc from compile time to set RTC
+	
+	bool is12Hour(); //Returns true if 12hour bit is set
+	bool isPM(); //Returns true if is12Hour and PM bit is set
+	void set12Hour();
+	void set24Hour();
+	
+	uint8_t status(); //Returns the status byte
+	
+	void setAlarm(uint8_t sec, uint8_t min, uint8_t hour, uint8_t date, uint8_t month, uint8_t day);
+	void setAlarm(uint8_t * time, uint8_t len);
+	//void setAlarmMode(uint8_t mode); //0 to 7, alarm goes off with match of second, minute, hour, etc
+ 	
+	//void setCountdownTimer(uint8_t duration, uint8_t unit, bool repeat = true, bool pulse = true);
+
+	void enableInterrupt(uint8_t source); //Enables a given interrupt within Interrupt Enable register
+	void disableInterrupt(uint8_t source); //Disables a given interrupt within Interrupt Enable register
+	void enableAlarmInterrupt(); //Use in conjuction with setAlarm and setAlarmMode
+	
+	void clearInterrupts();
+		
+	//Values in RTC are stored in Binary Coded Decimal. These functions convert to/from Decimal
+	uint8_t BCDtoDEC(uint8_t val); 
+	uint8_t DECtoBCD(uint8_t val);
+
+	void reset(void); //Fully reset RTC to all zeroes
     //These are the core functions that send a command to OpenLog
     void sendCommand(uint8_t registerNumber, char option1[]);
 	uint8_t readRegister(uint8_t address, uint8_t offset);
     void readRegisterRegion(uint8_t address, uint8_t *outputPointer , uint8_t offset, uint8_t length);
+	
+    void writeRegister(uint8_t addr, uint8_t regNum, uint8_t val);
+	void writeMultipleRegisters(uint8_t addr, uint8_t regNum, uint8_t * values, uint8_t len);
   private:
 
+	uint8_t _time[TIME_ARRAY_LENGTH];
     //Variables
     uint8_t _deviceAddress; //Keeps track of I2C address. setI2CAddress changes this.
     uint8_t _escapeCharacter = 26; //The character that needs to be sent to QOL to get it into command mode
--- enums.d.ts
+++ enums.d.ts
@@ -2,4 +2,15 @@
 declare namespace gatorLog {
 }
 
+
+    declare const enum time_order {
+    TIME_SECONDS = 0,
+    TIME_MINUTES = 1,
+    TIME_HOURS = 2,
+    TIME_DAY = 3,
+    TIME_DATE = 4,
+    TIME_MONTH = 5,
+    TIME_YEAR = 6,
+    }
+
 // Auto-generated. Do not edit. Really.
