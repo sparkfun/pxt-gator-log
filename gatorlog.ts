e979ae27ef630595d224a782b4119db163777d55
AndyEngland521 <andrew.england@sparkfun.com>
2019-07-09 16:34:34 -0600

    changing to Serial

--- gatorlog.ts
+++ gatorlog.ts
@@ -20,107 +20,84 @@
 namespace gatorLog {
     // Functions for reading Particle from the gatorlog in Particle or straight adv value
 	
-	//% weight=31 
+	//% weight=50 
 	//% blockId="gatorLog_begin" 
 	//% block="initialize gator:log"
-	//% shim=gatorLog::begin
 	export function begin(){
+		serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
 		return
 	}
 	
-	//% weight=30
+	//% weight=49
 	//% blockId="gatorLog_createFile"
 	//% block="create file named %value"
-	//% shim=gatorLog::createFile
 	export function createFile(value: string){
+		serial.writeString(",,,new ")
+		serial.writeLine(value)
 		return
 	}
 	
-	//% weight=30
-	//% blockId="gatorLog_openFile"
-	//% block="open file %value"
-	//% shim=gatorLog::openFile
-	export function openFile(value: string){
-		return
-	}
-	
-	//% weight=30
+	//% weight=48
 	//% blockId="gatorLog_writeStringData"
 	//% block="write string %value | to current file"
-	//% shim=gatorLog::writeStringData
 	export function writeStringData(value: string){
+		serial.writeLine(value)
 		return
 	}
 	
-	//% weight=30
-	//% blockId="gatorLog_writeNumberData"
-	//% block="write number %value | to current file"
-	export function writeNumberData(value: number){
-		writeStringData(value.toString())
-		return
-	}
-	
-	//% weight=30
+	//% weight=47
 	//% blockId="gatorLog_mkDirectory"
 	//% block="create directory with name %value""
-	//% shim=gatorLog::mkDirectory
 	export function mkDirectory(value: string){
+		serial.writeString(",,,md ")
+		serial.writeLine(value)
 		return
 	}
 	
-	//% weight=30
+	//% weight=46
 	//% blockId="gatorLog_chDirectory"
 	//% block="change to %value | directory"
-	//% shim=gatorLog::chDirectory
 	export function chDirectory(value: string){
+		serial.writeString(",,,cd ")
+		serial.writeLine(value)
 		return
 	}
 	
-	//% weight=30
-	//% blockId="gatorLog_sizeOFile"
+	//% weight=45
+	//% blockId="gatorLog_sizeOfFile"
 	//% block="get size of file with name %value"
-	//% shim=gatorLog::sizeOfFile
-	export function sizeOfFile(value: string): number{
-		return 0
+	export function sizeOfFile(value: string): string{
+		serial.writeString(",,,size ")
+		serial.writeLine(value)
+		return serial.readString()
 	}
 	
-	//% weight=30
-	//% blockId="gatorLog_search"
-	//% block="search current directory for %value"
-	//% shim=gatorLog::search
-	export function search(value: string): boolean{
-		return true;
+	//% weight=44
+	//% blockId="gatorLog_readFile"
+	//% block="get size of file with name %value"
+	export function readFile(value: string): string{
+		serial.writeString(",,,size ")
+		serial.writeLine(value)
+		return serial.readString()
 	}
 	
-	//% weight=30
-	//% blockId="gatorLog_getNextItem"
-	//% block="get name of next item"
-	//% shim=gatorLog::getNextItem
-	export function getNextItem(): string{
-		return "item";
-	}
-	
-	//% weight=30
+	//% weight=42
 	//% blockId="gatorLog_removeItem"
 	//% block="remove file %value"
 	//% shim=gatorLog::removeItem
 	export function removeItem(value: string){
+		serial.writeString(",,,rm ")
+		serial.writeLine(value)
 		return
 	}
 	
-	//% weight=30
+	//% weight=41
 	//% blockId="gatorLog_removeDir"
 	//% block="remove directory %value | and it's contents"
 	//% shim=gatorLog::removeDir
 	export function removeDir(value: string){
+		serial.writeLine(value)
+		serial.writeString(",,,rm -rf ")
 		return
-	}
-	
-	//% weight=30
-	//% blockId="gatorLog_getTimeString"
-	//% block="get time"
-	//% shim=gatorLog::getTime
-	export function getTimeString(): string{
-		return "item";
 	}
 }
