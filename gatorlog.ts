b6154585b9420205cbd5bef47417360f1717baf8
AndyEngland521 <andrew.england@sparkfun.com>
2019-07-18 11:57:28 -0600

    grouping and commenting

--- gatorlog.ts
+++ gatorlog.ts
@@ -22,7 +22,8 @@
 	RAW=3
  }
 
-//% color=#f44242 icon="\uf185"
+//% color=#f44242 
+//% icon="\uf70e"
 namespace gatorLog {
     // Functions for reading Particle from the gatorlog in Particle or straight adv value
 	
@@ -32,37 +33,7 @@ namespace gatorLog {
 	let newLine = String.fromCharCode(10)
 	let commandReady = ">"
 	let writeReady = "<"
-	
-	//% weight=50 
-	//% blockId="gatorLog_begin" 
-	//% block="initialize gator:log"
-	export function begin(){
-		basic.pause(2500)
-		serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
-		pins.digitalWritePin(DigitalPin.P13, 1)
-		basic.pause(100)
-		pins.digitalWritePin(DigitalPin.P13, 0)
-		basic.pause(100)
-		pins.digitalWritePin(DigitalPin.P13, 1)
-		serial.readUntil(writeReady)
-		basic.pause(20)
-		dummyFile()
-		return
-	}
-	
-	//% weight=49
-	//% blockId="gatorLog_openFile"
-	//% block="open file named %value"
-	export function openFile(value: string){
-		command()
-		serial.writeString("append " + value + carriageReturn)
-		serial.readUntil(writeReady)
-		basic.pause(20)
-		currentFile = value
-		commandMode = 0;
-		return
-	}	
-	
+		
 	//The only reason we have this function is so we don't set currentFile to DELETEME.txt
 	function dummyFile(){
 		command()
@@ -85,10 +56,112 @@ namespace gatorLog {
 		return
 	}
 	
-	//% blockId="gatorLog_writeStringData"
+	/**
+	* Initializes gator:log and waits until it says it is ready to be written to.
+	*/
+	//% weight=50 
+	//% blockId="gatorLog_begin" 
+	//% block="initialize gator:log"
+	//% blockGap=12
+	export function begin(){
+		basic.pause(2500)
+		serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
+		pins.digitalWritePin(DigitalPin.P13, 1)
+		basic.pause(100)
+		pins.digitalWritePin(DigitalPin.P13, 0)
+		basic.pause(100)
+		pins.digitalWritePin(DigitalPin.P13, 1)
+		serial.readUntil(writeReady)
+		basic.pause(20)
+		dummyFile()
+		return
+	}
+	
+	/**
+	* Opens the file with the name provided (don't forget to provide an extension). If the file does not exist, it is created.
+	*/
+	//% weight=49
+	//% blockId="gatorLog_openFile"
+	//% block="open file named %value"
+	//% blockGap=6
+	export function openFile(value: string){
+		command()
+		serial.writeString("append " + value + carriageReturn)
+		serial.readUntil(writeReady)
+		basic.pause(20)
+		currentFile = value
+		commandMode = 0;
+		return
+	}
+	
+	/**
+	* Removes the file with the provided name
+	*/
 	//% weight=48
+	//% blockId="gatorLog_removeItem"
+	//% block="remove file %value"
+	//% blockGap=6
+	export function removeItem(value: string){
+		command()
+		serial.writeString("rm " + value + carriageReturn)
+		serial.readUntil(commandReady)
+		basic.pause(20)
+		return
+	}
+	
+	/**
+	* Creates a folder. Note that this block does not open the folder that it creates
+	*/
+	//% weight=47
+	//% blockId="gatorLog_mkDirectory"
+	//% block="create folder with name %value""
+	//% blockGap=6
+	export function mkDirectory(value: string){
+		command()
+		serial.writeString("md " + value + carriageReturn)
+		serial.readUntil(commandReady)
+		basic.pause(20)
+		return
+	}
+	
+	/**
+	* Opens a folder. Note that the folder must already exist on your SD card. To go back to the root/home folder, use "Change to '..' folder"
+	*/
+	//% weight=46
+	//% blockId="gatorLog_chDirectory"
+	//% block="change to %value | folder"
+	//% blockGap=6
+	export function chDirectory(value: string){
+		command()
+		serial.writeString("cd " + value + carriageReturn)
+		serial.readUntil(commandReady)
+		basic.pause(20)
+		return
+	}
+	
+	/**
+	* Removes a folder
+	*/
+	//% weight=45
+	//% blockId="gatorLog_removeDir"
+	//% block="remove folder %value | and it's contents"
+	//% blockGap=16
+	export function removeDir(value: string){
+		command()
+		serial.writeString("rm -rf " + value + carriageReturn)
+		serial.readUntil(commandReady)
+		basic.pause(20)
+		return
+	}
+	
+	/**
+	* Writes a line of text to the current open file. If no file has been opened, this will be recorded to the LOGxxxx.txt folder
+	*/
+	//% blockId="gatorLog_writeLine"
+	//% weight=44
 	//% block="write line %value | to current file"
-	export function writeStringData(value: string){
+	//% blockGap=6
+	export function writeLine(value: string){
 		if (commandMode == 1)
 		{
 			serial.writeString("append " + currentFile + carriageReturn)
@@ -101,11 +174,35 @@ namespace gatorLog {
 		return
 	}
 	
+	/**
+	* Writes text to the current open file. If no file has been opened, this will be recorded to the LOGxxxx.txt folder
+	*/
+	//% blockId="gatorLog_writeText"
+	//% weight=44
+	//% block="write %value | to current file"
+	//% blockGap=6
+	export function writeText(value: string){
+		if (commandMode == 1)
+		{
+			serial.writeString("append " + currentFile + carriageReturn)
+			serial.readUntil(writeReady)
+			basic.pause(20)
+		}
+		serial.writeString(value)
+		commandMode = 0
+		basic.pause(20)
+		return
+	}
+	
+	/**
+	* Writes text to the current open file at the position specified. If no file has been opened, this will be recorded to the LOGxxxx.txt folder
+	*/
 	//% weight=30
-	//% blockId="gatorLog_writeStringDataOffset"
+	//% blockId="gatorLog_writeLineOffset"
 	//% block="write line %value | at position %offset"
 	//% advanced=true
-	export function writeStringDataOffset(value: string, offset: number){
+	//% blockGap=6
+	export function writeLineOffset(value: string, offset: number){
 		command()
 		serial.writeString("write " + currentFile + " " + String(offset) + carriageReturn)
 		serial.readUntil(writeReady)
@@ -119,44 +216,13 @@ namespace gatorLog {
 		return
 	}
 	
-	//% weight=47
-	//% blockId="gatorLog_mkDirectory"
-	//% block="create directory with name %value""
-	export function mkDirectory(value: string){
-		command()
-		serial.writeString("md " + value + carriageReturn)
-		serial.readUntil(commandReady)
-		basic.pause(20)
-		return
-	}
-	
-	//% weight=46
-	//% blockId="gatorLog_chDirectory"
-	//% block="change to %value | directory"
-	export function chDirectory(value: string){
-		command()
-		serial.writeString("cd " + value + carriageReturn)
-		serial.readUntil(commandReady)
-		basic.pause(20)
-		return
-	}
-	
-	//% weight=45
-	//% blockId="gatorLog_sizeOfFile"
-	//% block="get size of file with name %value"
-	export function sizeOfFile(value: string): string{
-		command()
-		serial.writeString("size " + value + carriageReturn)
-		serial.readUntil(newLine)//Use this and the readUntil(commandReady) to properly frame the openLogs response
-		let returnString = serial.readUntil(carriageReturn)
-		serial.readUntil(commandReady)
-		basic.pause(20)
-		return returnString
-	}
-	
-	//% weight=44
+	/**
+	* Read contents of the file with the specified name
+	*/
+	//% weight=43
 	//% blockId="gatorLog_readFile"
 	//% block="read file with name %value"
+	//% blockGap=6
 	export function readFile(value: string): string{
 		command()
 		serial.writeString("read " + value + carriageReturn)
@@ -167,10 +233,14 @@ namespace gatorLog {
 		return returnString
 	}
 	
+	/**
+	* Read data starting at the given position from the file with the specified name.
+	*/
 	//% weight=29
 	//% blockId="gatorLog_readFileOffset"
 	//% block="read from file with name %value | starting at position %offset"
 	//% advanced=true
+	//% blockGap=6
 	export function readFileOffset(value: string, offset: number): string{
 		command()
 		serial.writeString("read " + value + " " + String(offset) + carriageReturn)
@@ -181,10 +251,14 @@ namespace gatorLog {
 		return returnString
 	}
 	
+	/**
+	* Read a length of data starting at the given position from the file with the specified name.
+	*/
 	//% weight=28
 	//% blockId="gatorLog_readFileOffsetLength"
 	//% block="read %length | characters from file with name %value | starting at position %offset"
 	//% advanced=true
+	//% blockGap=6
 	export function readFileOffsetLength(value: string, length: number, offset: number): string{
 		command()
 		serial.writeString("read " + value + " " + String(offset) + " " + String(length) + carriageReturn)
@@ -195,6 +269,9 @@ namespace gatorLog {
 		return returnString
 	}
 	
+	/**
+	* Read a length of data starting at the given position from the file with the specified name. Type determines what format the data is returned in.
+	*/
 	//% weight=27
 	//% blockId="gatorLog_readFileOffsetLengthType"
 	//% block="read %length | characters from file with name %value | starting at position %offset | in output type %returnDataType"
@@ -209,25 +286,19 @@ namespace gatorLog {
 		return returnString
 	}
 	
+	/**
+	* Returns the size of the specified file
+	*/
 	//% weight=42
-	//% blockId="gatorLog_removeItem"
-	//% block="remove file %value"
-	export function removeItem(value: string){
+	//% blockId="gatorLog_sizeOfFile"
+	//% block="get size of file with name %value"
+	export function sizeOfFile(value: string): string{
 		command()
-		serial.writeString("rm " + value + carriageReturn)
+		serial.writeString("size " + value + carriageReturn)
+		serial.readUntil(newLine)//Use this and the readUntil(commandReady) to properly frame the openLogs response
+		let returnString = serial.readUntil(carriageReturn)
 		serial.readUntil(commandReady)
 		basic.pause(20)
-		return
-	}
-	
-	//% weight=41
-	//% blockId="gatorLog_removeDir"
-	//% block="remove directory %value | and it's contents"
-	export function removeDir(value: string){
-		command()
-		serial.writeString("rm -rf " + value + carriageReturn)
-		serial.readUntil(commandReady)
-		basic.pause(20)
-		return
+		return returnString
 	}
 }
